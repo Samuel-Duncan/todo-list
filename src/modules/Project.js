99682aa5f55a8a418cf7f11d7a4cc7b71d6281a0
Samuel Duncan <samuel.ira.duncan@gmail.com>
2023-08-29 17:44:55 -0400

    Add Project class module
    This commit implements the Project class module, which serves as a
    central entity for managing task lists in the todo application.
    The module offers methods for retrieving and setting project names,
    managing task collection, adding new tasks, retrieving specific tasks by
    title, and removing tasks.
    
    By introducing the Project module, the application gains enhanced
    organization and control over the task lists, streamlining the process
    of task management and manipulation. This structural addition sets the
    foundation for more efficient task tracking and user experience
    improvements.

--- src/modules/Project.js
+++ src/modules/Project.js
@@ -1,3 +1,44 @@
 // Generate project constructor class
 // - properties: name, todos
 // - methods: get/set project name, get/set todos, add/delete todos
+
+export default class Project {
+  constructor(name) {
+    this.name = name;
+    this.toDos = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setName(name) {
+    this.name = name;
+  }
+
+  getToDos() {
+    return this.toDos;
+  }
+
+  setToDos(toDos) {
+    this.toDos = toDos;
+  }
+
+  getToDo(toDoTitle) {
+    return this.toDos.find((toDo) => toDo.getName() === toDoTitle);
+  }
+
+  isOnToDoList(toDoTitle) {
+    return this.toDos.some((toDo) => toDo.title === toDoTitle.title);
+  }
+
+  addToDo(newToDo) {
+    if (!this.isOnToDoList(newToDo)) {
+      this.toDos.push();
+    }
+  }
+
+  deleteToDo(toDoTitle) {
+    this.toDos = this.toDos.filter((toDo) => toDo.title !== toDoTitle.title);
+  }
+}
